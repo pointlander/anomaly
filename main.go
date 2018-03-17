766a435d653db160e748d215cd9e455bfdbf35f3
Andrew Snodgrass <git@pointlander.info>
2018-03-17 17:17:05 -0600

    Anomaly detection

--- main.go
+++ main.go
@@ -11,12 +11,38 @@ import (
 	"hash/fnv"
 	"math"
 	"math/rand"
+
+	"github.com/pointlander/neural"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 const (
 	symbols    = 'z' - 'a'
 	vectorSize = 1024
+	samples    = 1000
 )
+
+var tests = []string{`{
+ "alfa": [
+  {"alfa": "1"},
+	{"bravo": "2"}
+ ],
+ "bravo": [
+  {"alfa": "3"},
+	{"bravo": "4"}
+ ]
+}`, `{
+ "a": [
+  {"a": "aa"},
+	{"b": "bb"}
+ ],
+ "b": [
+  {"a": "aa"},
+	{"b": "bb"}
+ ]
+}`}
 
 func generateJSON() map[string]interface{} {
 	sample := func(stddev float64) int {
@@ -121,6 +147,34 @@ func hashJSON(object map[string]interface{}) []int64 {
 	return hash
 }
 
+func sigmoid32(x float32) float32 {
+	return 1 / (1 + float32(math.Exp(-float64(x))))
+}
+
+func normalize(a []int64) []float32 {
+	sum := 0.0
+	for _, v := range a {
+		sum += float64(v) * float64(v)
+	}
+	sum = math.Sqrt(sum)
+	b := make([]float32, len(a))
+	for i, v := range a {
+		b[i] = sigmoid32(float32(v) / float32(sum))
+	}
+	return b
+}
+
+func similarity(a, b []float32) float64 {
+	dot, xx, yy := 0.0, 0.0, 0.0
+	for i, j := range b {
+		x, y := float64(a[i]), float64(j)
+		dot += x * y
+		xx += x * x
+		yy += y * y
+	}
+	return dot / math.Sqrt(xx*yy)
+}
+
 var images = flag.Bool("images", false, "run images demo")
 
 func main() {
@@ -130,12 +184,95 @@ func main() {
 		imagesDemo()
 	}
 
-	object := generateJSON()
-	data, err := json.MarshalIndent(object, "", " ")
-	if err != nil {
-		panic(err)
+	rand.Seed(1)
+
+	config := func(n *neural.Neural32) {
+		n.Init(neural.WeightInitializer32FanIn, vectorSize, vectorSize/2, vectorSize)
 	}
-	fmt.Println(string(data))
-	hash := hashJSON(object)
-	fmt.Println(hash)
+	nn := neural.NewNeural32(config)
+	context := nn.NewContext()
+
+	values, sims := make(plotter.Values, samples), make(plotter.Values, samples)
+	for i := 0; i < samples; i++ {
+		object := generateJSON()
+		hash := hashJSON(object)
+		input := normalize(hash)
+
+		context.SetInput(input)
+		context.Infer()
+		outputs := context.GetOutput()
+		sims[i] = float64(similarity(input, outputs))
+
+		source := func(iterations int) [][][]float32 {
+			data := make([][][]float32, 1)
+			data[0] = [][]float32{input, input}
+			return data
+		}
+		e := nn.Train(source, 1, 0.6, 0.4)
+		values[i] = float64(e[0])
+	}
+	values = values[10:]
+	sims = sims[10:]
+
+	plot := func(title, name string, values plotter.Values) {
+		p, err := plot.New()
+		if err != nil {
+			panic(err)
+		}
+		p.Title.Text = title
+
+		h, err := plotter.NewHist(values, 20)
+		if err != nil {
+			panic(err)
+		}
+		h.Normalize(1)
+		p.Add(h)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, name)
+		if err != nil {
+			panic(err)
+		}
+	}
+	plot("Output", "output.png", values)
+	plot("Sims", "sims.png", sims)
+
+	sum, sumSquared, length := 0.0, 0.0, float64(len(values))
+	for _, v := range values {
+		value := float64(v)
+		sum += value
+		sumSquared += value * value
+	}
+	average := sum / length
+	stddev := math.Sqrt(sumSquared/length - average*average)
+	fmt.Printf("avg=%v stddev=%v\n", average, stddev)
+
+	for _, test := range tests {
+		var object map[string]interface{}
+		err := json.Unmarshal([]byte(test), &object)
+		if err != nil {
+			panic(err)
+		}
+		hash := hashJSON(object)
+		input := normalize(hash)
+
+		context.SetInput(input)
+		context.Infer()
+		outputs := context.GetOutput()
+		sim := float64(similarity(input, outputs))
+		fmt.Printf("sim before=%v\n", sim)
+
+		source := func(iterations int) [][][]float32 {
+			data := make([][][]float32, 1)
+			data[0] = [][]float32{input, input}
+			return data
+		}
+		e := nn.Train(source, 1, 0.6, 0.4)
+		fmt.Println((float64(e[0]) - average) / stddev)
+
+		context.SetInput(input)
+		context.Infer()
+		outputs = context.GetOutput()
+		sim = float64(similarity(input, outputs))
+		fmt.Printf("sim after=%v\n", sim)
+	}
 }
